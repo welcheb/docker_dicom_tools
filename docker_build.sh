21ac9bd0c71a8775c250ae0ff279991d24db0034
welcheb <brian.welch@vanderbilt.edu>
2016-03-26 09:47:09 -0500

    flattening does not save very much, 1GB to 700 MB image history, has little effect on tar.gz size, and breaks dicom_tools_run.sh

--- docker_build.sh
+++ docker_build.sh
@@ -9,5 +9,8 @@ CONTAINER_ID=`docker create $TAG`
 docker export $CONTAINER_ID | gzip -9 > ./docker_save/$REPO.tar.gz
 docker rm $CONTAINER_ID
 
-# load from flattened image
-docker load < ./docker_save/$REPO.tar.gz
+# remove image before replacing it with flattened version
+docker rmi -f $TAG
+
+# import from flattened image
+docker import ./docker_save/$REPO.tar.gz $TAG:latest
