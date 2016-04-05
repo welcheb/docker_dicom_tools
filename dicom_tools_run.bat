59158a3bff2765c70a068adcaf0bae862aa20cf8
Brian Welch <brian.welch@vanderbilt.edu>
2016-04-05 09:46:25 -0500

    making some changes

--- dicom_tools_run.bat
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# present working directory
-DIR=`pwd`
-
-# run dicom_tools docker container
-# volume mount connects scratch directory on container to the present working directory on host
-docker run -v "$DIR":/scratch welcheb/dicom_tools -c "$*"
--- dicom_tools_run.sh
+++ dicom_tools_run.sh
@@ -5,4 +5,7 @@ DIR=`pwd`
 
 # run dicom_tools docker container
 # volume mount connects scratch directory on container to the present working directory on host
-docker run -v "$DIR":/scratch welcheb/dicom_tools -c "$*"
+# the --rm option causes the container created to run the image to be automatically removed upon exit
+# the -v maps a volume between the host and the container
+# the -c passes the command arguments to execute
+docker run --rm -v "$DIR":/scratch welcheb/dicom_tools -c "$*"
